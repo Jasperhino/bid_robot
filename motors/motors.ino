c951eca744b9745a20c695538ac0a55125046ac0
jasper.blum <jasper.blum@student.hpi.uni-potsdam.de>
2019-01-30 22:28:12 +0100

    added pwm to the pwm to drive motors slower

--- motors/motors.ino
+++ motors/motors.ino
@@ -9,25 +9,29 @@ int motor2_A=6;
 int motor2_B=4;
 int motor2_Speed=A1;
 
-int motor_power = 130;
-int motor_speed = 0;
+byte motor_power = 255;
+bool pwm_on = false;
+int motor_pwm_on = 10;
+int motor_pwm_off = 0;
+long motor_on_timer;
+long motor_off_timer;
+
+long brain_timer = 0;
+long brain_time = 1000;
 
 enum drive_status : byte {
   D_LEFT,
   D_RIGHT,
+  D_SPIN_LEFT,
+  D_SPIN_RIGHT,
   D_FORWARD,
   D_BACKWARD,
   D_IDLE
 };
 
-enum drive_direction : byte {
-  NORTH,
-  SOUTH
-};
-
 drive_status current_drive_status = D_IDLE;
 
-drive_status testdrive[] = {D_IDLE, D_FORWARD, D_BACKWARD, D_LEFT, D_RIGHT};
+drive_status testdrive[] = {D_IDLE, D_LEFT, D_IDLE, D_RIGHT};//{D_IDLE, D_FORWARD, D_BACKWARD, D_LEFT, D_RIGHT, D_SPIN_LEFT, D_SPIN_RIGHT};
 byte dindex = 0;
 
 void setup(){
@@ -40,19 +44,27 @@ void setup(){
   pinMode(motor2_A,OUTPUT);
   pinMode(motor2_B,OUTPUT);
   pinMode(motor2_Speed, OUTPUT);
+
+
+  motor_on_timer = millis();
+  motor_off_timer = millis() + motor_pwm_on; 
 }
  
 void loop(){
-  //readSerial();
-  brain();
+  readSerial();
+  //brain();
   drive();
-  delay(1000);
 }
 
 void brain(){
-  current_drive_status = testdrive[dindex];
-  dindex++;
-  if(dindex > 4) dindex = 0;
+  //Serial.println(brain_timer);
+  if(millis() > brain_timer + brain_time){
+    current_drive_status = testdrive[dindex];
+    //print_drive_status();
+    dindex++;
+    if(dindex > 6) dindex = 0;
+    brain_timer = millis();
+  }
 }
 
 void readSerial(){
@@ -63,53 +75,83 @@ void readSerial(){
       case 'a': current_drive_status = D_LEFT; break;
       case 's': current_drive_status = D_BACKWARD; break;
       case 'd': current_drive_status = D_RIGHT; break;
+      case 'e': current_drive_status = D_SPIN_RIGHT; break;
+      case 'q': current_drive_status = D_SPIN_LEFT; break;
       default: current_drive_status = D_IDLE; break;
     }
-    Serial.print("DRIVING_STATUS: ");
-    Serial.println(get_drive_status_string(current_drive_status));
+    print_drive_status();
   }
 }
 
 void drive() {
+ if(!pwm_on){
+  drive_in_current_direction();
+  return;
+ }
+ if(millis() > motor_off_timer){
+  drive_idle();
+  motor_off_timer = millis() + motor_pwm_on + motor_pwm_off;
+ }
+ if(millis() > motor_on_timer){
+  drive_in_current_direction();
+  motor_on_timer = millis() + motor_pwm_on + motor_pwm_off;
+ }
+}
+
+void drive_in_current_direction(){
   switch (current_drive_status) {
     case D_FORWARD: drive_forward(); break;
     case D_BACKWARD: drive_backward(); break;
-    case D_LEFT: spin_left(); break;
-    case D_RIGHT: spin_right(); break;
+    case D_LEFT: drive_left_forward(); break;
+    case D_RIGHT: drive_right_forward(); break;
+    case D_SPIN_LEFT: spin_left(); break;
+    case D_SPIN_RIGHT: spin_right(); break;
     case D_IDLE: drive_idle(); break;
     default: break;
   }
 }
 
 void drive_forward(){
-  drive_left(NORTH);
-  drive_right(NORTH);
+  drive_left_forward();
+  drive_right_forward(); 
 }
 
 void drive_backward(){
-  drive_left(SOUTH);
-  drive_right(SOUTH);
+  drive_left_backward();
+  drive_right_backward();
 }
 
-void drive_left(drive_direction d){
-  byte a = LOW;
-  byte b= HIGH;
-  if(d == SOUTH){
-    a = HIGH; b = LOW;
-  }
-  digitalWrite(motor1_A,a); // A = HIGH and B = LOW means the motor will turn right
-  digitalWrite(motor1_B,b);
+void spin_left(){
+  drive_right_backward();
+  drive_left_forward();
+}
+
+void spin_right(){
+  drive_right_forward();
+  drive_left_backward();
+}
+
+void drive_left_forward(){
+  digitalWrite(motor1_A,LOW); // A = HIGH and B = LOW means the motor will turn right
+  digitalWrite(motor1_B,HIGH);
   analogWrite(motor1_Speed, motor_power);
 }
 
-void drive_right(drive_direction d){
-    byte a = HIGH;
-    byte b = LOW;
-  if(d == SOUTH){
-    a = LOW; b = HIGH;
-  }
-  digitalWrite(motor2_A, a); // A = HIGH and B = LOW means the motor will turn right
-  digitalWrite(motor2_B, b);
+void drive_left_backward(){
+  digitalWrite(motor1_A,HIGH); // A = HIGH and B = LOW means the motor will turn right
+  digitalWrite(motor1_B,LOW);
+  analogWrite(motor1_Speed, motor_power);
+}
+
+void drive_right_forward(){
+  digitalWrite(motor2_A, HIGH); // A = HIGH and B = LOW means the motor will turn right
+  digitalWrite(motor2_B, LOW);
+  analogWrite(motor2_Speed, motor_power);
+}
+
+void drive_right_backward(){
+  digitalWrite(motor2_A, LOW); // A = HIGH and B = LOW means the motor will turn right
+  digitalWrite(motor2_B, HIGH);
   analogWrite(motor2_Speed, motor_power);
 }
 
@@ -121,25 +163,17 @@ void drive_idle(){
   digitalWrite(motor2_B,LOW);
   analogWrite(motor2_Speed, 0);
 }
-
-void spin_left(){
-  drive_right(SOUTH);
-  drive_left(NORTH);
-}
-
-void spin_right(){
-  drive_right(NORTH);
-  drive_left(SOUTH);
-}
-
-String get_drive_status_string (drive_status ds) {
+void print_drive_status() {
   String result = "not a drive status";
-  switch (ds) {
+  switch (current_drive_status) {
     case D_FORWARD: result = "Forward"; break;
     case D_BACKWARD: result = "Backward"; break;
     case D_LEFT: result = "Left"; break;
     case D_RIGHT: result = "Right"; break;
+    case D_SPIN_LEFT: result = "Spin Left"; break;
+    case D_SPIN_RIGHT: result = "Spin Right"; break;
     case D_IDLE: result = "Idle"; break;
+    default: break;
   }
-  return result;
+  Serial.println(result);
 }
