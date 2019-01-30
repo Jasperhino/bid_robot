e402fd114170026fbee00f32a09c13930f75965f
Kronossos <alek10000@gmail.com>
2019-01-30 21:14:12 +0100

    kmeans added

--- alek/alek.ino
+++ alek/alek.ino
@@ -4,7 +4,14 @@ int pin_led_left = 3;
 int pin_led_right = 2;
 
 
-byte light_data[200];
+int data_colection_size=200;
+
+byte* light_data=new byte[data_colection_size];
+
+
+
+
+byte mean_table[3];
 
 void setup() {
   Serial.begin(9600);
@@ -12,16 +19,36 @@ void setup() {
   pinMode(pin_lightsensor_right, INPUT_PULLUP);
   pinMode(pin_led_left, OUTPUT);
   pinMode(pin_led_right, OUTPUT);
+//  collectData();
+
+  test();
+
 }
 
 byte light_val_left;
 byte light_val_right;
 
-void loop() {
-  collectData();
-  updateLightsensors();
-  monitorBrightness();
+
+
+
+void test(){
+  Serial.println("START");
+  byte test[200];
+  for (int i = 35; i < 235; i++){
+    test[i-35]=i;
+  }
+  k3_mean(test,200);
+  print_array(test,200);
+
+  Serial.println(find_category(123));
+  Serial.println("DONE");
+
   
+}
+
+void loop() {
+//  updateLightsensors();
+//  monitorBrightness();
 }
 
 void monitorBrightness(){
@@ -39,6 +66,7 @@ void monitorBrightness(){
 }
 
 void updateLightsensors(){
+  delay(10);
   light_val_left = char(analogRead(pin_lightsensor_left));
   light_val_right = char(analogRead(pin_lightsensor_right));
   Serial.print("Left: ");
@@ -62,10 +90,81 @@ void collectData(){
   
   sort(light_data,200);
   
-  print_array(light_data);
+  print_array(light_data,200);
   delay(3000);
 }
 
+
+byte find_category(byte point){
+  byte dist=abs(point-mean_table[0]);
+//  Serial.println(dist);
+  if (dist>abs(point-mean_table[1])){
+    dist=abs(point-mean_table[1]);
+    if (dist>abs(point-mean_table[2])){
+      return(2);
+    }
+    return(1);
+  }
+  return(0);
+}
+
+
+
+void k3_mean(byte points[], int size){ 
+    
+  mean_table[0]=size*0.30;
+  mean_table[1]=size*0.50;
+  mean_table[2]=size*0.70;
+
+
+
+  byte cat_table[size];
+  for (int i;i<size;i++){
+      cat_table[i]=-1;
+  }
+    
+  bool flag=true;
+
+  while(flag){
+
+    byte white_counter = 0;
+    byte grey_counter = 0;
+    byte black_counter = 0;
+    
+    int mean_table_temp[3] = {0,0,0};
+    
+    print_array(mean_table,3);
+    flag=false;
+    
+    for(int i=0;i<size;i++){
+      byte new_category = find_category(points[i]);
+
+     switch (new_category){
+      case 0:
+        white_counter++;
+        break;
+      case 1:
+        grey_counter++;
+        break;
+      default:
+        black_counter++;
+    }
+    
+      mean_table_temp[new_category]+=points[i];
+      
+      if (cat_table[i]!=new_category){
+        flag=true;
+        cat_table[i]=new_category;
+      }
+    }
+    mean_table[0]=mean_table_temp[0]/white_counter;
+    mean_table[1]=mean_table_temp[1]/grey_counter;
+    mean_table[2]=mean_table_temp[2]/black_counter;
+    
+
+    
+  }
+}
 
 void sort(byte a[], int size) {
     for(int i=0; i<(size-1); i++) {
@@ -79,12 +178,12 @@ void sort(byte a[], int size) {
     }
 }
 
-void print_array(byte table[]){
-  Serial.println(sizeof(table[0]));
+void print_array(byte table[],int size){
+//  Serial.println(sizeof(table[0]));
   Serial.print("[");
-  for(int i = 0; i < 200; i++){
+  for(int i = 0; i < size; i++){
     Serial.print(" ");
     Serial.print(table[i]);
   }
-  Serial.print("]");
+  Serial.println("]");
 }
